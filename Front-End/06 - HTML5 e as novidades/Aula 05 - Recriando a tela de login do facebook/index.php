2e162ca9b3ac57f1563877cef2168dc8bf02c16e
livegames2011 <livegames2011@hotmail.com>
2020-03-31 10:31:58 -0300

    Curso Front-End Completo

---
+++ Front-End/06 - HTML5 e as novidades/Aula 05 - Recriando a tela de login do facebook/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login facebook</title>
+	<!--Aplicar depois as meta tags-->
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="center">
+			<div class="logo">
+				<h2>Facebook</h2>
+			</div><!--logo-->
+			<form method="post" class="form-login">
+				<div class="form-element">
+					<p>E-mail ou telefone:</p>
+					<input type="email" />
+				</div><!--form-element-->
+				<div class="form-element">
+					<p>Senha:</p>
+					<input type="password" />
+				<div class="form-element">
+					<input type="submit" name="acao" value="Enviar" />
+				</div><!--form-element-->
+			</form><!--form-login-->
+			<div class="clear"></div>
+		</div><!--center-->
+	</header>
+</body>
+
+</html>
